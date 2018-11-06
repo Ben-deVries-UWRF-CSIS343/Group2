ab060932c5ad6a0fba9f0549c1bc32891d4521b3
Peter Schaber <pschaber@MSC.local>
2018-11-06 16:01:34 -0600

    Updating Frontend file structure and gradle wrapper

--- Project-Name-Frontend/project-name/src/app/app.component.spec.ts
+++ Project-Name-Frontend/project-name/src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'project-name'`, () => {
+  it(`should have as title 'angular-esri-map'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('project-name');
+    expect(app.title).toEqual('angular-esri-map');
   });
 
   it('should render title in a h1 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to project-name!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to angular-esri-map!');
   });
 });
