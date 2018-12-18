ee3dcece671789165c233752a316b290bc34fe07
Peter Schaber <peter.schaber@thomsonreuters.com>
2018-12-18 06:07:57 -0600

    grabbing coordinates off of map and css

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { AppService } from './app.service';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Component } from '@angular/core';
 
@@ -8,7 +9,7 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient, private appService: AppService) { }
 
   ngOnInit() {
 
@@ -18,20 +19,21 @@ export class AppComponent {
         'Authorization': 'my-auth-token'
       })
     };
+    this.showAddresses();
 
-    const obs1 = this.http.get('http://localhost:8080/address');
-    obs1.subscribe((response) => console.log(response));
+    // const obs2 = this.http.post<Map<String, String>>('http://localhost:8080/address', {
+    //   id: '101010',
+    //   name: 'Peter-POST',
+    //   addressLine1: 'addressLine1-POST',
+    //   addressLine2: 'addressLine2-POST',
+    //   zipcode: 'zipcode-POST',
+    //   state: 'state-POST',
+    //   city: 'city-POST'
+    // }, httpOptions);
+    // obs2.subscribe((response) => console.log(response));
+  }
 
-
-    const obs2 = this.http.post<Map<String, String>>('http://localhost:8080/address', {
-      id: '101010',
-      name: 'Peter-POST',
-      addressLine1: 'addressLine1-POST',
-      addressLine2: 'addressLine2-POST',
-      zipcode: 'zipcode-POST',
-      state: 'state-POST',
-      city: 'city-POST'
-    }, httpOptions);
-    obs2.subscribe((response) => console.log(response));
+  showAddresses() {
+    this.appService.getAddress().subscribe((response) => console.log(response));
   }
 }
--- client/src/app/sidenav/sidenav.component.ts
+++ client/src/app/sidenav/sidenav.component.ts
@@ -1,3 +1,4 @@
+import { AppService } from './../app.service';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -9,10 +10,65 @@ import { Component, OnInit } from '@angular/core';
 export class SidenavComponent implements OnInit {
 
   title = 'Project Name';
+  mapView: any;
+  startLatitude = 0.0;
+  startLongitude = 0.0;
+  endLatitude = 0.0;
+  endLongitude = 0.0;
+  counter = 0;
 
-  constructor() { }
+  constructor(private appService: AppService) { }
 
   ngOnInit() {
   }
 
+  onClick(event: any) {
+    this.counter++;
+
+    if (this.counter === 1) {
+      this.addStartCoordinates(event.x, event.y);
+    } else if (this.counter === 2) {
+      this.addEndCoordinates(event.x, event.y);
+    } else {
+      this.counter = 0;
+      this.removeMarkers();
+    }
+  }
+
+  calculateRoute(startLat: number, startLong: number, endLat: number, endLong: number) {
+    console.log('Calculating route ...');
+    this.appService.addAddress();
+
+  }
+
+  addStartCoordinates(x: number, y: number) {
+    // Converts x and y coordinates to latitude and longitude
+    const obj = this.mapView.toMap(x, y);
+    this.startLatitude = obj.latitude;
+    this.startLongitude = obj.longitude;
+  }
+
+  addEndCoordinates(x: number, y: number) {
+    // Converts x and y coordinates to latitude and longitude
+    const obj = this.mapView.toMap(x, y);
+    this.endLatitude = obj.latitude;
+    this.endLongitude = obj.longitude;
+  }
+
+  removeMarkers() {
+    console.log('Resetting coordinates ...');
+    this.resetCoordinates();
+  }
+
+  resetCoordinates() {
+    this.startLatitude = 0.0;
+    this.startLongitude = 0.0;
+    this.endLatitude = 0.0;
+    this.endLongitude = 0.0;
+  }
+
+  // The MapView should be typed to the esri.MapView
+  saveMapView(mapView: any) {
+    this.mapView = mapView;
+  }
 }
